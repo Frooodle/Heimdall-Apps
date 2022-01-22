aa3bb59b9e5237a0cf9bea58388b41fc33454690
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-01-22 00:46:23 +0000

    Update SpeedtestTracker.php

--- SpeedtestTracker/SpeedtestTracker.php
+++ SpeedtestTracker/SpeedtestTracker.php
@@ -5,7 +5,7 @@ class SpeedtestTracker extends \App\SupportedApps implements \App\EnhancedApps {
     public $config;
 
 
-	public function test()
+    public function test()
     {
         $test = parent::appTest($this->url('latest'));
         echo $test->status;
@@ -16,20 +16,19 @@ class SpeedtestTracker extends \App\SupportedApps implements \App\EnhancedApps {
 
         $status = 'inactive';
         $details = json_decode(parent::execute($this->url('latest'))->getBody());
-		
+
         $data['dlSpeed'] = round($details->data->download ?? 0, 0);
-		$data['upSpeed'] = round($details->data->upload ?? 0, 0) ;
-		$data['ping'] = round($details->data->ping ?? 999, 0);
+    	$data['upSpeed'] = round($details->data->upload ?? 0, 0) ;
+    	$data['ping'] = round($details->data->ping ?? 999, 0);
 
         return parent::getLiveStats($status, $data);
-        
+
     }
 
     public function url($endpoint)
     {
-		$api_url = parent::normaliseurl($this->config->url).'api/speedtest/'.$endpoint;
+    	$api_url = parent::normaliseurl($this->config->url).'api/speedtest/'.$endpoint;
         return $api_url;
     }
-	
-	
+
 }
