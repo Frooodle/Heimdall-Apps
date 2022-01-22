19d330efda4e189d2b631ba7677d1a4c8a861ef5
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-01-22 00:44:08 +0000

    Add files via upload

---
+++ SpeedtestTracker/livestats.blade.php
@@ -0,0 +1,14 @@
+<ul class="livestats">
+    <li>
+        <span class="title">DL Speed:</span>
+        <strong>{!! $dlSpeed !!}</strong>
+    </li>
+    <li>
+        <span class="title">UP Speed:</span>
+        <strong>{!! $upSpeed !!}</strong>
+    </li>
+	<li>
+        <span class="title">Ping:</span>
+        <strong>{!! $ping !!}</strong>
+    </li>
+</ul>
